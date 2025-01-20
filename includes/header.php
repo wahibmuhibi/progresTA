c6a62fccc464f1dcd6bc4cae013a071fcc07c3e8
Wahib Muhibi Nur <wahibmuhibi@gmail.com>
2025-01-20 12:18:30 +0700

    edit manage_criteria ke manage_kriteria

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,7 @@
                         <li class="nav-item"><a class="nav-link" href="/tugasakhir/views/admin/manage_users.php">Manajemen Pengguna</a></li>
                         <li class="nav-item"><a class="nav-link" href="/tugasakhir/views/admin/manage_questions.php">Manajemen Pertanyaan</a></li>
                         <li class="nav-item"><a class="nav-link" href="/tugasakhir/views/admin/manage_standard.php">Manajemen Standard</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/tugasakhir/views/admin/manage_criteria.php">Manajemen Kriteria</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/tugasakhir/views/admin/manage_kriteria.php">Manajemen Kriteria</a></li>
                         <li class="nav-item"><a class="nav-link" href="/tugasakhir/views/admin/log_aktivitas.php">Log Aktivitas</a></li>
                     <?php elseif ($_SESSION['role'] === 'Manajemen TI'): ?>
                         <li class="nav-item"><a class="nav-link" href="/tugasakhir/views/manajemen_ti/assessment_form.php">Self-Assessment</a></li>
